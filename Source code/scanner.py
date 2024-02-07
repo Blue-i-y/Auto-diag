4f89bedba262be46daf3ee39f2ba5f61a6cebb2e
Blue_I <74364353+Blue-i-y@users.noreply.github.com>
2024-02-07 16:15:13 +0100

    Update scanner.py

--- Source code/scanner.py
+++ Source code/scanner.py
@@ -1,84 +1,111 @@
-import nmap, socket, pyfiglet
-from netdiscover import *
-from utiles import *
+import json, paramiko,time
+from threading import Thread
+from ftplib import FTP
+from network import Network
 
-class Network:
+class Scanner(Network):
     def __init__(self):
-        print(pyfiglet.figlet_format("Auto-Diag"))
-        while True : 
-            self.ip = input("Enter an IP address (press ENTER to use the default IP address):\n" f"{socket.gethostbyname(socket.gethostname())}\n>")
-            if check_ip_address(self.ip):
-                break
-            else:
-                print_red(f"L'adresse IP {self.ip} n'est pas au bon format IPv4.")
+        super().__init__()
+
+    def nmap_scan(self, host):
+        print(f"\nNmap scan in progress for: {host}")
+        scan_result = self.nm.scan(hosts=host, arguments='-sV -T4 -sC -O')
+
+        filename = f"{host.replace('.', '_')}.json"
+        with open(filename, 'w') as file:
+            json.dump(scan_result, file)
+
         
-        self.hosts = []
-        self.nm = nmap.PortScanner()
+    def print_result(self, host):
+        print("Hostname : {}".format(self.nm[host].hostname()))
+        print("PORT\tSTATE\tSERVICE")
+        for i in range(20, 450):
+            try:
+                if self.nm[host]["tcp"][i]:
+                    print("{}/tcp\t{}\t{}".format(i, self.nm[host]["tcp"][i]["state"], self.nm[host]["tcp"][i]["name"]))
+                    print(" | Product : {}".format(self.nm[host]["tcp"][i]["product"]))
+                    if self.nm[host]["tcp"][i]["script"]:
+                        print(" | Script :")
+                        for script in self.nm[host]["tcp"][i]["script"]:
+                            print(" | | {} : {}".format(script, self.nm[host]["tcp"][i]["script"][script]))
+                    print(" |_Version : {}".format(self.nm[host]["tcp"][i]["version"]))
+            except:
+                pass
+        print(f"\nAnalyse Nmap finie pour {host}")
 
-    def section_print(self, title):
-        print("\n" + "=" * 50)
-        print(title)
-        print("=" * 50 + "\n")
+    def service_detection(self, host):
+        if self.nm[host].has_tcp(22):
+            print(f"\n Hôte \t {host} \n Port ssh (22) ouvert. \n Lancement d'un bruteforce sur cet hôte.")
+            self.bruteforce(host, "ssh")
+        elif self.nm[host].has_tcp(21):
+            print(f"\n Hôte \t {host} \n Port ftp (21) ouvert. \n Lancement d'un bruteforce sur cet hôte.")
+            self.bruteforce(host, "ftp")
 
+    def ssh_connect(self, ip, username, password, port=22):
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(ip, port, username, password)
+            print("Mot de passe trouvé : " + password)
+            return True
+        except:
+            return False
 
-    def discover_hosts(self):
-        while True:
-            mask = input(f"Enter your mask : ")
-            if check_subnet_mask(mask):
-                break
-            else : 
-                print_red("le maque n'est pas dans le bon format. Entrer un nombre entre 1 et 32")
+    def ftp_connect(self, ip, user, password):
+        try:
+            FTP(ip, user = user, passwd = password)
+            print("Mot de passe trouvé : " + password)
+            return True
+        except:
+            return False
+            
+    def bruteforce(self, ip, type):
+        username = str(input("Entrer un nom d'utilisateur : \n >"))
+        wordl = str(input("Entrer un dictionnaire de mots de passe (juste le nom du fichier, sans l'extension) : \n >"))
 
-        if len(self.ip) == 0:
-            network = f"{socket.gethostbyname(socket.gethostname())}/{mask}"
-        else:
-            network = self.ip + f'/{mask}'
-
-        print(f"\nyour IP address is {network}")
-        print("host enumeration en cours ...")
-
-        disc = Discover()
-        hosts = disc.scan(ip_range=network)
-        if (not disc):
-            print("there is no alive hosts")
-
-        for i in hosts:
-            ip = i["ip"]
-            mac = i["mac"]
-            if ip != self.ip:
-                print(f"\n{ip} --> {mac} : Host is UP")
-                if ip == "10.0.2.15":
-                    self.hosts.append(ip)
-
-    """def network_scanner(self):
-        mask = input(f"Enter your mask : ")
-        if len(self.ip) == 0:
-            network = f"{socket.gethostbyname(socket.gethostname())}/{mask}"
-        else:
-            network = self.ip + f'/{mask}'
-        print(f"\nyour IP address is {network}")
-        print("\nScan réseau en cours ...")
-        self.nm.scan(hosts = network, arguments = "-Pn -F")
-        print("\n nm scan finished")
-        hosts_list = [(x, self.nm[x]['status']['state']) for x in self.nm.all_hosts()]
-
-        print("=" * 50)
-        for host, status in hosts_list:
-            print("Hôte\t{}\t{}".format(host, status))
-            self.hosts.append(host)
-        print("=" * 50)
-        print(hosts_list)"""
-
-
+        with open(f"/usr/share/dirb/wordlists/{wordl}.txt", 'r', encoding = "utf8") as file:
+            for line in file.readlines():
+                if type == "ssh":
+                    th = Thread(target = self.ssh_connect ,args = (ip, username, line.strip()))
+                    th.start()
+                elif type == "ftp":
+                    th = Thread(target = self.ftp_connect ,args = (ip, username, line.strip()))
+                    th.start()
 
     def Auto_Diag(self):
         try:
             self.discover_hosts()
 
-            # Create an instance of the Scanner class
-            scanner = Scanner()
-            scanner.Auto_Diag()
+            for i in range(len(self.hosts)):
+                host = self.hosts[i]
+
+                self.section_print(f"Port scan for {host}")
+                
+                try:
+                    self.nmap_scan(host)
+                    self.print_result(host)
+                    self.service_detection(host)
+                    # self.save_results_to_word(host)  
+
+                except nmap.PortScannerError as e:
+                    print(f"Error during Nmap scan for {host}: {e}")
+
+                except Exception as e:
+                    print(f"Error during nmap scan for {host}: {e}")
+
+                time.sleep(1)
 
         except KeyboardInterrupt:
             print("\n[x] Program closed!")
 
+"""
+    def cve_finder(self):
+        try:
+            cve_entry = str(input("\nSaisissez un code CVE pour votre recherche:\n>"))
+            cve_result = self.cve.id(cve_entry)
+
+            with open(f"cve/{cve_entry}.json", "w", encoding = "utf-8") as f:
+                f.write(json.dumps(cve_result, indent = 4, sort_keys = True))
+        except:
+            pass
+"""
